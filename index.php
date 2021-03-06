8c0fb66edc4b84284e5f803b15578051c4de6c8c
Carolina <carolina.kaji@gmail.com>
2021-03-06 10:19:05 -0300

    Listar vídeos procurados

--- index.php
+++ index.php
@@ -14,14 +14,13 @@
 </head>
 
 <body>
+
+  <img class="thumb-hover" src=" https://picsum.photos/id/237/200/300">
+
   <nav>
     <div class="nav-wrapper container">
-      <a href="#" class="brand-logo">YoutubeList</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
+      <a href="#" class="brand-logo center">YoutubeList</a>
+
     </div>
   </nav>
   <div class="container main">
--- php/listaVideos.php
+++ php/listaVideos.php
@@ -1,35 +1,44 @@
 <?php
+$cards = '';
+
+
+
+
 
 if (isset($_POST['search'])) {
   $subject = isset($_POST['subject']) ? $_POST['subject'] : '';
 
-  $urlSearch = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=100&q={$subject}&videoEmbeddable=any&type=video&key=AIzaSyCdCeOw3VmIIqOfXyyJq8dO_IQRRpX5OpA";
+  //$urlSearch = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=100&q={$subject}&videoEmbeddable=any&type=video&key=AIzaSyCdCeOw3VmIIqOfXyyJq8dO_IQRRpX5OpA";
+  $urlSearch = "https://youtube.googleapis.com/youtube/v3/search?part=snippet,id&channelType=any&maxResults=30&q={$subject}&videoEmbeddable=any&type=video&key=AIzaSyB4tQnGjkbAvdjWR_Nl7T8hF8MgYcsfpGs";
 
-  $urlVideo = "https://youtube.googleapis.com/youtube/v3/videos?part=player&chart=mostPopular&maxResults=6&key=AIzaSyCdCeOw3VmIIqOfXyyJq8dO_IQRRpX5OpA";
+  //$urlVideo = "https://youtube.googleapis.com/youtube/v3/videos?part=player&chart=mostPopular&maxResults=6&key=AIzaSyCdCeOw3VmIIqOfXyyJq8dO_IQRRpX5OpA";
   // transforma para string
   $jsonSearch = file_get_contents($urlSearch);
-  $jsonVideo = file_get_contents($urlVideo);
+  //$jsonVideo = file_get_contents($urlVideo);
   // decodifica uma string JSON
   $apiDataSearch = json_decode($jsonSearch);
-  $apiDataVideo = json_decode($jsonVideo);
+  //$apiDataVideo = json_decode($jsonVideo);
 
-  $cont = 0;
+
+
+  for ($cont = 0; $cont < 30; $cont++) {
+    $cards = $apiDataSearch->items[$cont][id][videoId];
+  }
+
   foreach ($apiDataSearch->items as $video) {
 
-    $cont++;
     $cards .=
       "<div class='col s12 m6 l4'>
       <div class='card card-height'>
         <div class='card-image'>
-          <img src='{$video->snippet->thumbnails->medium->url}'>
-          <a class='btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>play_arrow</i></a>
+        <a href='https://www.youtube.com/watch?v={$video->id->videoId}' target='_blank'><img class='thumb-hover' src='{$video->snippet->thumbnails->medium->url}'></a>
+          <a class='btn-floating halfway-fab waves-effect waves-light red'  href='https://www.youtube.com/watch?v={$video->id->videoId}' target='_blank'><i class='material-icons'>play_arrow</i></a>
           </div>
           <div class='card-content'>
-          <span class='card-title'>{$video->snippet->title}</span>
-          <p>{$video->snippet->description}</p>
+          <span class='card-title'><a href='https://www.youtube.com/watch?v={$video->id->videoId}' target='_blank'>{$video->snippet->title}</a></span>
+          <p><a href='https://www.youtube.com/watch?v={$video->id->videoId}' target='_blank'>{$video->snippet->description}</a></p>
         </div>
       </div>
-      <p>{$cont}</p>
     </div>";
   }
 }
